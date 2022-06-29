f351020369a80ba9e21e72af3b77b0e333680ba3
khaledHalhouli <101004067+khaledHalhouli@users.noreply.github.com>
2022-06-29 22:03:27 +0300

    start protflio

---
+++ protifile/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MainPage from './component/MainPage';
+import NavBar from "./component/NavBar/index"
+function App() {
+ 
+  return (
+    <div className="App">
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ protifile/src/component/MainPage/index.js
@@ -0,0 +1,16 @@
+
+import "./style.css"
+
+const MainPage=()=>{
+
+
+    return <div className="mainPage">
+        <div className="leftSide">
+            <h1 id="name">Hello,<br/> I am Khaled Halhouli</h1>
+            <h2 id="about">Full Stack Developer</h2>
+        </div>
+        <div className="rightSide"></div>
+    </div>
+}
+
+export default MainPage
